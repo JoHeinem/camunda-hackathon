b44a877b8bbca974e172a31c42faa6ab5195371f
MarioJaeger <E4i!k1hj>
2019-01-26 16:39:06 +0100

    Added external archive task.

--- src/main/java/external/tasks/ArchiveTask.java
+++ src/main/java/external/tasks/ArchiveTask.java
@@ -1,4 +1,30 @@
 package external.tasks;
 
+import org.camunda.bpm.client.ExternalTaskClient;
+
 public class ArchiveTask {
+
+    public static void main(String... args) throws InterruptedException {
+        // bootstrap the client
+        ExternalTaskClient client = ExternalTaskClient.create()
+                .baseUrl("http://localhost:8080/rest")
+                .build();
+
+        // subscribe to the topic
+        client.subscribe("Archive")
+                .lockDuration(1000)
+                .handler((externalTask, externalTaskService) -> {
+
+                    externalTaskService.complete(externalTask);
+
+                    System.out.println("The External Archive Task " + externalTask.getId() + " has been completed!");
+
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        System.out.println("The External Archive Task was interrupted.");
+                    }
+
+                }).open();
+    }
 }
