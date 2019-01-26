79c8a83c9b4d4bf49da37321b2dde373c707bc8b
MarioJaeger <E4i!k1hj>
2019-01-26 16:39:06 +0100

    Added external archive task.

--- src/main/java/org/camunda/bpm/spring/boot/example/webapp/delegate/SendMailDelegate.java
+++ src/main/java/org/camunda/bpm/spring/boot/example/webapp/delegate/SendMailDelegate.java
@@ -1,6 +1,9 @@
 package org.camunda.bpm.spring.boot.example.webapp.delegate;
 
-import java.io.UnsupportedEncodingException;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+/**import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -20,11 +23,15 @@ import org.springframework.boot.autoconfigure.mail.MailProperties;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Component;*/
 
-@Component
+//@Component
 public class SendMailDelegate implements JavaDelegate {
+  @Override
+  public void execute(DelegateExecution delegateExecution) throws Exception {
 
+  }
+/**
   private static final String CONTEXT_KEY_BASE_URL = "baseUrl";
 
   @Inject MailProperties mailProperties;
@@ -268,5 +275,5 @@ public class SendMailDelegate implements JavaDelegate {
 
   private boolean isSimulation() {
     return Boolean.parseBoolean(mailProperties.getProperties().get("simulation"));
-  }
+  }*/
 }
